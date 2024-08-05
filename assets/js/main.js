488c1392fdce1d0e1fce527b8d56d86cdd2cc447
Rahul8968 <123576710+Rahul8968@users.noreply.github.com>
2024-08-06 00:29:22 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,10 +48,17 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2000,
     delay: 200,
-//     reset: true
+    // reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+// Add ScrollReveal to different sections
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', {delay: 400}); 
+sr.reveal('.home__social-icon', { interval: 200 }); 
+sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
+
+// New section effects
+sr.reveal('.work__item', { delay: 200 }); 
+sr.reveal('.projects__item', { delay: 300 }); 
+sr.reveal('.leadership__item', { delay: 400 }); 
+sr.reveal('.contact__form', { delay: 500 });
